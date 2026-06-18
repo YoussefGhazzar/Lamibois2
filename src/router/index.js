c653b655954346d055934778b10c9798b425c264
saad <ghalisaad31@gmail.com>
2026-06-18 09:57:18 +0100

    Initial commit: Lamibois project structure

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+const routes = [{ path: '/', name: 'home', component: HomePage }]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+export default router
---
+++ views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <TheNavbar />
+    <HeroSection />
+    <ProductsSection />
+    <AboutSection />
+    <WhyUsSection />
+    <ContactSection />
+    <TheFooter />
+  </div>
+</template>
+
+<script setup>
+import TheNavbar      from '@/components/TheNavbar.vue'
+import HeroSection    from '@/components/HeroSection.vue'
+import ProductsSection from '@/components/ProductsSection.vue'
+import AboutSection   from '@/components/AboutSection.vue'
+import WhyUsSection   from '@/components/WhyUsSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
+import TheFooter      from '@/components/TheFooter.vue'
+</script>
+
+<style scoped>
+.home { width: 100%; }
+</style>
